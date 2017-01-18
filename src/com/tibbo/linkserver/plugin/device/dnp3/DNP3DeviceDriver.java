4904994355028e8e0eca8cef2117ccb2088e4a88
rura <rura@ruramint>
2017-01-18 15:37:49 +0600

    текущее состояние

---
+++ src/com/tibbo/linkserver/plugin/device/dnp3/DNP3DeviceDriver.java
@@ -0,0 +1,247 @@
+package com.tibbo.linkserver.plugin.device.dnp3;
+
+/*
+ * Собственно драйвер устройств ВЛР для Aggregate
+ */
+import com.tibbo.aggregate.common.Log;
+import com.tibbo.aggregate.common.context.CallerController;
+import com.tibbo.aggregate.common.context.ContextException;
+import com.tibbo.aggregate.common.context.ContextUtils;
+import com.tibbo.aggregate.common.context.VariableDefinition;
+import com.tibbo.aggregate.common.datatable.DataRecord;
+import com.tibbo.aggregate.common.datatable.DataTable;
+import com.tibbo.aggregate.common.datatable.FieldFormat;
+import com.tibbo.aggregate.common.datatable.TableFormat;
+import com.tibbo.aggregate.common.device.AbstractDeviceDriver;
+import com.tibbo.aggregate.common.device.DeviceContext;
+import com.tibbo.aggregate.common.device.DeviceEntities;
+import com.tibbo.aggregate.common.device.DeviceException;
+import com.tibbo.aggregate.common.device.DisconnectionException;
+import com.tibbo.aggregate.common.security.ServerPermissionChecker;
+import java.sql.Timestamp;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import ruraomsk.list.ru.cthulhu.MainMaster;
+import ruraomsk.list.ru.cthulhu.OneReg;
+import ruraomsk.list.ru.cthulhu.Registers;
+import ruraomsk.list.ru.cthulhu.pocket.MasterDC;
+import ruraomsk.list.ru.cthulhu.pocket.MasterFS;
+import ruraomsk.list.ru.strongsql.ParamSQL;
+import ruraomsk.list.ru.strongsql.SetValue;
+import ruraomsk.list.ru.strongsql.StrongSql;
+import ruraomsk.list.ru.vlrmanager.VLRDataTableManager;
+import ruraomsk.list.ru.vlrmanager.VLRXMLManager;
+
+/**
+ *
+ * @author Yury Rusinov <ruraomsl@list.ru at Automatics E>
+ */
+public class DNP3DeviceDriver extends AbstractDeviceDriver {
+
+    Registers masterregisters = null;
+    ParamSQL param = null;
+    StrongSql sSql = null;
+    VLRXMLManager vlrManager = null;
+    MainMaster mainmaster = null;
+    List<MasterFS> fsmaster;
+    List<MasterDC> dcmaster;
+    Long timestep;
+    Long stepSQL;
+    StrongSql sqldata;
+    int needReadAllValues=0;
+
+    public DNP3DeviceDriver() {
+        super("vlr", VlrHelper.VFT_SQL_PROP);
+    }
+
+    @Override
+    public void setupDeviceContext(DeviceContext deviceContext) throws ContextException {
+        super.setupDeviceContext(deviceContext); //To change body of generated methods, choose Tools | Templates.
+        deviceContext.setDefaultSynchronizationPeriod(10000L);
+        VariableDefinition vd = new VariableDefinition("connectionProperties", VlrHelper.VFT_SQL_PROP, true, true, "Основные параметры", ContextUtils.GROUP_ACCESS);
+        vd.setIconId("var_connection");
+        vd.setHelpId("ls_drivers_vlr");
+        vd.setWritePermissions(ServerPermissionChecker.getManagerPermissions());
+        deviceContext.addVariableDefinition(vd);
+
+        vd = new VariableDefinition("canels", VlrHelper.VFT_CNL_PROP, true, true, "Каналы", ContextUtils.GROUP_ACCESS);
+        vd.setWritePermissions(ServerPermissionChecker.getAdminPermissions());
+        deviceContext.addVariableDefinition(vd);
+
+        vd = new VariableDefinition("devices", VlrHelper.VFT_VLR_PROP, true, true, "Устройства", ContextUtils.GROUP_ACCESS);
+        vd.setWritePermissions(ServerPermissionChecker.getAdminPermissions());
+        deviceContext.addVariableDefinition(vd);
+
+        vd = new VariableDefinition("registers", VlrHelper.VFT_REGISTERS, true, true, "Переменные", ContextUtils.GROUP_ACCESS);
+        vd.setWritePermissions(ServerPermissionChecker.getAdminPermissions());
+        deviceContext.addVariableDefinition(vd);
+
+        makeRegisters(deviceContext);
+    }
+
+    /**
+     *
+     * @param name
+     */
+    @Override
+    public void accessSettingUpdated(String name) {
+        if (name.equalsIgnoreCase("canels") || name.equalsIgnoreCase("devices")) {
+            try {
+                makeRegisters(getDeviceContext());
+            } catch (ContextException ex) {
+                Log.CORE.error("Ошибка редакции " + name + " " + ex.getMessage());
+            }
+        }
+        super.accessSettingUpdated(name); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public List<VariableDefinition> readVariableDefinitions(DeviceEntities entities) throws ContextException, DeviceException, DisconnectionException {
+        List<VariableDefinition> result = new ArrayList<>();
+        for (DataRecord reg : getDeviceContext().getVariable("registers", getDeviceContext().getCallerController())) {
+            boolean writeble = !reg.getBoolean("readonly");
+            String name = reg.getString("name");
+            TableFormat tf = new TableFormat(1, 1, FieldFormat.create(name, VlrHelper.setCharType(reg.getInt("type")), reg.getString("description")));
+            tf.setUnresizable(true);
+            result.add(new VariableDefinition(name, tf, true, writeble, reg.getString("description"), "remote"));
+        }
+        return result;
+    }
+
+    @Override
+    public DataTable readVariableValue(VariableDefinition vd, CallerController caller) throws ContextException, DeviceException, DisconnectionException {
+        DataTable result = new DataTable(vd.getFormat());
+        OneReg oreg = masterregisters.getOneReg(vd.getName());
+        if (oreg == null) {
+            result.addRecord(0);
+        } else {
+            result.addRecord(oreg.getValue());
+        }
+        return result;
+    }
+
+    @Override
+    public void writeVariableValue(VariableDefinition vd, CallerController caller, DataTable value, DataTable deviceValue) throws ContextException, DeviceException, DisconnectionException {
+        OneReg oreg = masterregisters.getOneReg(vd.getName());
+        if (oreg == null) {
+            return;
+        }
+        oreg.setValue(value.get());
+        int canel = masterregisters.getContoller(vd.getName());
+        for (MasterDC dc : dcmaster) {
+            if (dc.getController() == canel) {
+                dc.putValue(oreg);
+                return;
+            }
+        }
+    }
+
+    @Override
+    public void finishSynchronization() throws DeviceException, DisconnectionException {
+        ArrayList<SetValue> arrayValues = new ArrayList<>();
+        for (MasterDC dc : dcmaster) {
+            for (OneReg oreg : masterregisters.getOneRegs(dc.getController())) {
+                Integer newID = dc.getController() << 16 | oreg.getuId();
+                Object value = oreg.getValue();
+                arrayValues.add(new SetValue(newID, oreg.getTime(), value, oreg.getGood()));
+                for (OneReg horeg : masterregisters.getHistoryOneReg(dc.getController(), oreg.getuId())) {
+                    value = horeg.getValue();
+                    arrayValues.add(new SetValue(newID, horeg.getTime(), value, oreg.getGood()));
+
+                }
+            }
+        }
+        sqldata.addValues(new Timestamp(System.currentTimeMillis()), arrayValues);
+        if(++needReadAllValues>9){
+            needReadAllValues=0;
+            for (MasterDC dc : dcmaster) {
+                dc.readAllNotSendingValue();
+                dc.readAllSendingValue();
+            }
+            
+        }
+        
+        super.finishSynchronization(); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void connect() throws DeviceException {
+        try {
+            DataTable canels = getDeviceContext().getVariable("canels", getDeviceContext().getCallerController());
+            DataTable devices = getDeviceContext().getVariable("devices", getDeviceContext().getCallerController());
+            param = VlrHelper.setParamData(getDeviceContext().getVariable("connectionProperties", getDeviceContext().getCallerController()));
+            stepSQL = getDeviceContext().getVariable("connectionProperties", getDeviceContext().getCallerController()).rec().getLong("stepSQL");
+            sqldata = new StrongSql(param, stepSQL);
+            timestep = getDeviceContext().getVariable("connectionProperties", getDeviceContext().getCallerController()).rec().getLong("timestep");
+            mainmaster = new MainMaster(masterregisters, timestep);
+            fsmaster = new ArrayList<>();
+            dcmaster = new ArrayList<>();
+            for (DataRecord dev : devices) {
+                if (dev.getBoolean("slip")) {
+                    mainmaster.addController(dev.getInt("canel"), dev.getString("IPAddress"), dev.getInt("port"), true);
+                } else {
+                    mainmaster.addController(dev.getInt("canel"), dev.getString("IPAddress"), dev.getInt("port"));
+                }
+            }
+            for (DataRecord can : canels) {
+                fsmaster.add(new MasterFS(mainmaster, can.getInt("canel"), timestep));
+                dcmaster.add(new MasterDC(mainmaster, can.getInt("canel"), timestep));
+            }
+            for (MasterDC dc : dcmaster) {
+                dc.readAllNotSendingValue();
+                dc.readAllSendingValue();
+            }
+            for(MasterFS fs:fsmaster){
+                fs.readResources();
+                fs.startKeepAlive();
+            }
+
+        } catch (ContextException ex) {
+            throw new DeviceException("Нет canels или devices");
+        }
+        super.connect();
+    }
+
+    @Override
+    public void disconnect() throws DeviceException {
+        for (MasterFS fs : fsmaster) {
+            fs.stopAll();
+        }
+        for (MasterDC dc : dcmaster) {
+            dc.stopAll();
+        }
+        mainmaster.stopAll();
+        sqldata.disconnect();
+        super.disconnect();
+    }
+
+    private void makeRegisters(DeviceContext deviceContext) throws ContextException {
+        boolean writeflag = false;
+        param = VlrHelper.setParamVlr(deviceContext.getVariable("connectionProperties", getDeviceContext().getCallerController()));
+        if (vlrManager != null) {
+            vlrManager.close();
+        }
+        vlrManager = new VLRXMLManager(param);
+        if (!vlrManager.connected) {
+            Log.CORE.error("Отсутствует соединение " + param.toString());
+        }
+        DataTable canels = deviceContext.getVariable("canels", getDeviceContext().getCallerController());
+        masterregisters = new Registers();
+        for (DataRecord crec : canels) {
+            DataTable BPO, SPO, Const;
+            BPO = VLRDataTableManager.fromXML(vlrManager.getXML(crec.getInt("bpo"), 1));
+            SPO = VLRDataTableManager.fromXML(vlrManager.getXML(crec.getInt("spo"), 2));
+            Const = VLRDataTableManager.fromXML(vlrManager.getXML(crec.getInt("spo"), 3));
+            VlrHelper.addRegisters(masterregisters, crec.getInt("canel"), crec.getString("prefix"), BPO, SPO, Const);
+            writeflag = true;
+        }
+        if (writeflag) {
+            deviceContext.setVariable("registers", getDeviceContext().getCallerController(), VlrHelper.makeRegisters(masterregisters, canels));
+        }
+        vlrManager.close();
+        vlrManager = null;
+    }
+
+}
---
+++ src/com/tibbo/linkserver/plugin/device/dnp3/VlrHelper.java
@@ -0,0 +1,189 @@
+/*
+ * Класс где собраны все утилиты помощи для драйвера
+ */
+package com.tibbo.linkserver.plugin.device.dnp3;
+
+import com.tibbo.aggregate.common.datatable.DataRecord;
+import com.tibbo.aggregate.common.datatable.DataTable;
+import com.tibbo.aggregate.common.datatable.FieldFormat;
+import static com.tibbo.aggregate.common.datatable.FieldFormat.create;
+import com.tibbo.aggregate.common.datatable.TableFormat;
+import com.tibbo.aggregate.common.datatable.validator.LimitsValidator;
+import com.tibbo.aggregate.common.datatable.validator.ValidatorHelper;
+import java.util.Date;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import ruraomsk.list.ru.cthulhu.OneReg;
+import ruraomsk.list.ru.cthulhu.Register;
+import ruraomsk.list.ru.cthulhu.Registers;
+import ruraomsk.list.ru.cthulhu.Util;
+import ruraomsk.list.ru.strongsql.ParamSQL;
+
+/**
+ *
+ * @author Yury Rusinov <ruraomsl@list.ru at Automatics E>
+ */
+class VlrHelper {
+
+    public static TableFormat VFT_SQL_PROP;
+    public static TableFormat VFT_VLR_PROP;
+    public static TableFormat VFT_CNL_PROP;
+    public static TableFormat VFT_REGISTERS;
+
+    static {
+        VFT_SQL_PROP = new TableFormat(1, 1);
+        VFT_SQL_PROP.addField(FieldFormat.create("<JDBCDriver><S><A=org.postgresql.Driver><D=Драйвер БД>"));
+        VFT_SQL_PROP.addField(FieldFormat.create("<url><S><A=jdbc:postgresql://192.168.1.76:5432/vlrbase><D=URL БД>"));
+        VFT_SQL_PROP.addField(FieldFormat.create("<user><S><A=postgres><D=Пользователь БД>"));
+        VFT_SQL_PROP.addField(FieldFormat.create("<password><S><A=162747><D=Пароль БД>"));
+        VFT_SQL_PROP.addField(FieldFormat.create("<setups><S><A=vlr><D=Имя таблицы с настройками ВЛР>"));
+        VFT_SQL_PROP.addField(FieldFormat.create("<datas><S><A=data><D=Префикс имен таблиц для хранения истории переменных>"));
+        VFT_SQL_PROP.addField(FieldFormat.create("<timestep><L><A=500><D=Основной квант времени>"));
+        VFT_SQL_PROP.addField(FieldFormat.create("<stepSQL><L><A=5000><D=Интервал сохранения переменных в БД >"));
+
+        VFT_CNL_PROP = new TableFormat();
+        VFT_CNL_PROP.addField(FieldFormat.create("<canel><I><D=Номер канала>"));
+        VFT_CNL_PROP.addField(FieldFormat.create("<bpo><I><D=Номер версии БПО>"));
+        VFT_CNL_PROP.addField(FieldFormat.create("<spo><I><D=Номер версии СПО>"));
+        VFT_CNL_PROP.addField(FieldFormat.create("<prefix><S><D=Префикс имен переменных>"));
+
+        VFT_VLR_PROP = new TableFormat();
+        VFT_VLR_PROP.addField(FieldFormat.create("<canel><I><D=Номер канала>"));
+        VFT_VLR_PROP.addField(FieldFormat.create("<IPAddress><S><D=IP адресс устройства >"));
+        VFT_VLR_PROP.addField(FieldFormat.create("<port><I><D=Номер порта>"));
+        VFT_VLR_PROP.addField(FieldFormat.create("<slip><B><A=false><D=Истина если SLIP>"));
+
+        VFT_REGISTERS = new TableFormat();
+        FieldFormat ff = FieldFormat.create("<name><S><D=Имя переменной>");
+        ff.getValidators().add(ValidatorHelper.NAME_LENGTH_VALIDATOR);
+        ff.getValidators().add(ValidatorHelper.NAME_SYNTAX_VALIDATOR);
+        VFT_REGISTERS.addField(ff);
+        ff = FieldFormat.create("<canel><I><D=Номер канала>");
+        VFT_REGISTERS.addField(ff);
+        ff = FieldFormat.create("<id><I><D=Номер переменной>");
+        VFT_REGISTERS.addField(ff);
+        ff = FieldFormat.create("<description><S><D=Описание>");
+        ff.getValidators().add(new LimitsValidator(1, 600));
+        ff.getValidators().add(ValidatorHelper.DESCRIPTION_SYNTAX_VALIDATOR);
+        VFT_REGISTERS.addField(ff);
+        ff = FieldFormat.create("<type><I><D=Тип переменной>");
+        ff.setSelectionValues(typeSelectionValues());
+        VFT_REGISTERS.addField(ff);
+        VFT_REGISTERS.addField(create("<send><B><A=false><D=Флаг Send>"));
+        VFT_REGISTERS.addField(create("<arch><B><A=false><D=Флаг Arch>"));
+        VFT_REGISTERS.addField(create("<eprom><B><A=false><D=Флаг Eprom>"));
+        VFT_REGISTERS.addField(create("<readonly><B><A=false><D=Только чтение>"));
+    }
+
+    private static Map typeSelectionValues() {
+        Map types = new LinkedHashMap();
+        types.put(0, "Boolean");
+        types.put(1, "Integer");
+        types.put(2, "Float");
+        types.put(3, "Long");
+        types.put(4, "One byte");
+        return types;
+    }
+
+    public static void addRegisters(Registers masterregisters, Integer canel, String prefix, DataTable BPO, DataTable SPO, DataTable Const) {
+        int lastid = 0;
+        Register reg;
+        for (DataRecord bpo : BPO) {
+            lastid = Math.max(lastid, bpo.getInt("id"));
+            reg = new Register(bpo.getInt("id"), bpo.getInt("type"));
+            reg.setArchived(bpo.getBoolean("arch"));
+            reg.setEprom(bpo.getBoolean("eprom"));
+            reg.setSending(bpo.getBoolean("send"));
+            reg.setConstant(!reg.isEprom());
+            masterregisters.addNewRegister(canel, reg, prefix + bpo.getString("name"));
+            masterregisters.addDescription(canel, reg.getuId(), bpo.getString("name")+":"+bpo.getString("description"));
+        }
+        int nextid = lastid;
+        for (DataRecord spo : SPO) {
+            reg = new Register(spo.getInt("id") + lastid, spo.getInt("type"));
+            nextid = Math.max(nextid, spo.getInt("id") + lastid);
+            reg.setArchived(spo.getBoolean("arch"));
+            reg.setEprom(spo.getBoolean("eprom"));
+            reg.setSending(spo.getBoolean("send"));
+            reg.setConstant(!reg.isEprom());
+            masterregisters.addNewRegister(canel, reg, prefix + spo.getString("name"));
+            masterregisters.addDescription(canel, reg.getuId(), spo.getString("name")+":"+spo.getString("description"));
+        }
+        for (DataRecord cd : Const) {
+            reg = new Register(nextid++, cd.getInt("type"));
+            reg.setConstant(true);
+            masterregisters.addNewRegister(canel, reg, prefix + cd.getString("name"));
+            masterregisters.addDescription(canel, reg.getuId(),cd.getString("name")+":"+cd.getString("description"));
+            Object value = 0;
+            switch (cd.getInt("type")) {
+                case 0:
+                    value = Boolean.parseBoolean(cd.getString("value"));
+                    break;
+                case 1:
+                    value = Integer.parseInt(cd.getString("value"));
+                    break;
+                case 2:
+                    value = Float.parseFloat(cd.getString("value"));
+                    break;
+                case 3:
+                    value = Long.parseLong(cd.getString("value"));
+                    break;
+            }
+            OneReg oreg = new OneReg(new Date(System.currentTimeMillis()), reg, value, Util.CT_DATA_GOOD);
+            masterregisters.changeRegister(canel, oreg);
+        }
+    }
+
+    static public DataTable makeRegisters(Registers masterregisters, DataTable canel_prop) {
+        DataTable table = new DataTable(VFT_REGISTERS);
+        for (DataRecord cp : canel_prop) {
+            for (OneReg oreg : masterregisters.getOneRegs(cp.getInt("canel"))) {
+                DataRecord reg = table.addRecord();
+                reg.setValue("name", masterregisters.getNameReg(cp.getInt("canel"), oreg.getuId()));
+                reg.setValue("canel", cp.getInt("canel"));
+                reg.setValue("id", oreg.getuId());
+                String str=masterregisters.getDescription(cp.getInt("canel"), oreg.getuId());
+                if(str==null || str.length()<1)str="+";
+                reg.setValue("description",str );
+                reg.setValue("type", oreg.getReg().getType());
+                reg.setValue("send", oreg.getReg().isSending());
+                reg.setValue("arch", oreg.getReg().isArchived());
+                reg.setValue("eprom", oreg.getReg().isEprom());
+                reg.setValue("readonly", oreg.getReg().isConstant());
+
+            }
+        }
+
+        return table;
+    }
+    public static ParamSQL setParamVlr(DataTable SQLProp){
+        ParamSQL param=new ParamSQL();
+        DataRecord rec=SQLProp.rec();
+        param.JDBCDriver=rec.getString("JDBCDriver");
+        param.url=rec.getString("url");
+        param.user=rec.getString("user");
+        param.password=rec.getString("password");
+        param.myDB=rec.getString("setups");
+        return param;
+    } 
+    public static ParamSQL setParamData(DataTable SQLProp){
+        ParamSQL param=new ParamSQL();
+        DataRecord rec=SQLProp.rec();
+        param.JDBCDriver=rec.getString("JDBCDriver");
+        param.url=rec.getString("url");
+        param.user=rec.getString("user");
+        param.password=rec.getString("password");
+        param.myDB=rec.getString("datas");
+        return param;
+    } 
+    static public Character setCharType(int type){
+        switch(type){
+            case 0: return 'B';
+            case 1: return 'I';
+            case 2: return 'F';
+            case 3: return 'L';
+            case 4: return 'S';
+        }
+        return 'S';
+    } 
+}
